2852548eaa6245320b0bc7f423e8594683ea4325
bender-at-large <bender_at_large@hotmail.com>
2025-07-19 18:05:59 +0200

    Test SSH push

--- obj/Debug/net8.0/HelloOled.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloOled.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloOled")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6e1698162efc50c393e44758912538dc764b9a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloOled")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloOled")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
